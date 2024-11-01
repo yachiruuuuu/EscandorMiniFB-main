ba46d1e35d9953dbea1d0f5e7181455734d02ad6
Cezar Escandor <162781412+yachiruuuuu@users.noreply.github.com>
2024-11-01 15:29:27 +0800

    Initial commit

---
+++ MiniFB-main/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name="post_title")
+    content = models.CharField(max_length=250, verbose_name="post_content")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
